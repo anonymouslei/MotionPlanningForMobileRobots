6f3ee4463db2ddc17cae946378f3c32c4598eac3
Lei Ge <lei.ge@student.kit.edu>
2020-03-15 11:21:00 +0100

    add getCt and getM

--- hw_5/matlab/getCt.m
+++ hw_5/matlab/getCt.m
@@ -1,9 +1,40 @@
 function Ct = getCt(n_seg, n_order)
     %#####################################################
     % STEP 2.1: finish the expression of Ct
-    %
-    %
-    %
-    %
-    %
+    row = 8*n_seg; 
+    col = 4*(n_seg+1);
+    Ct = zeros(row,col);
+    % fixed derivatives
+    % first point correspond
+    for i = 1:4
+        Ct(i,i) = 1;
+    end
+    % end point correspond
+    for i = 1:4 
+        Ct(row-4+i,4+i) = 1;
+    end
+    % correspond the position at t=0 of the middle point
+    for i = 1:n_seg-1
+        Ct(i*8+1, 8+i) = 1;
+        % previous position at t = T equals next position at t = 0
+        Ct(i*8-3, 8+i) = 1;
+    end
+
+    % free derivatives
+    % correspond the v,a,j at t=0 of the middle point
+    for i = 1:n_seg-1
+        % v, there are 4+4+n_segment numbers in front of free derivatives 
+        Ct(i*8+2, 8+n_seg+3*i) = 1;
+        % previous v at t = T equals next velocity at t = 0
+        Ct(i*8-2, 8+n_seg+3*i) = 1;
+        % a
+        Ct(i*8+3, 8+n_seg+3*i+1) = 1;
+        % previous a at t = T equals next acceleration at t = 0
+        Ct(i*8-2, 8+n_seg+3*i+1) = 1;
+        % j
+        Ct(i*8+4, 8+n_seg+3*i+2) = 1;
+        % previous j at t = T equals next jerk at t = 0
+        Ct(i*8-2, 8+n_seg+3*i+2) = 1;
+   end
+
 end
--- hw_5/matlab/getM.m
+++ hw_5/matlab/getM.m
@@ -2,12 +2,25 @@ function M = getM(n_seg, n_order, ts)
     M = [];
     for k = 1:n_seg
         M_k = [];
+        M_k = zeros(8,n_order+1);
         %#####################################################
         % STEP 1.1: calculate M_k of the k-th segment 
-        %
-        %
-        %
-        %
+        M_k(1,n_order+1) = 1;
+        M_k(2,n_order) = 1;
+        M_k(3,n_order-1) = 2;
+        M_k(4,n_order-2) = 6;
+        for i = 1:n_order+1
+            M_k(5,i) = ts(k)^(n_order+1-i);
+        end
+        for i = 1:n_order
+            M_k(6,i) = (n_order+1-i)*ts(k)^(n_order-i);
+        end
+        for i = 1:n_order-1
+            M_k(7,i) = (n_order+1-i)*(n_order-i)*ts(k)^(n_order-i-1);
+        end
+        for i = 1:n_order-2
+            M_k(8,i) = (n_order+1-i)*(n_order-i)*(n_order-1-i)*ts(k)^(n_order-i-2);
+        end
         M = blkdiag(M, M_k);
     end
 end
